eef44b2ad9c8d29781b40e90f8b280b3b4a50e3f
Seungjin Lee <lsg0385@naver.com>
2022-01-11 03:38:58 +0000

    [add] test codes for evals

--- evals.py
+++ evals.py
@@ -18,21 +18,28 @@ def evaluate(config, model, dataset, savepath, epoch):
     device = get_device()
 
     example_num = 5
-    save_idx = random.sample(range(len(dataset)), example_num)
-    example_path = os.path.join(savepath, 'examples')
-    makedir(example_path)
+    if savepath != '':
+        save_idx = random.sample(range(len(dataset)), example_num)
+        example_path = os.path.join(savepath, 'examples')
+        makedir(example_path)
     si_sdris = []
+    si_snris = []
 
     model.eval()
     with torch.no_grad():
         with tqdm(dataset) as pbar: # 데이터마다 길이가 달라서 dataloader 사용 불가
             for inputs in pbar:
                 if 'rir' in dataset.task:
-                    mix, sources, idx, cleanmix, clean = inputs
+                    if config.model == '':
+                        mix, sources, idx, _, clean = inputs
+                    else:
+                        mix, sources, idx, _, clean, distance = inputs
+                        distance = distance.to(device)
+                    mix, sources, clean = mix.to(device), sources.to(device)[None], clean.to(device)[None]
                 else:
-                    mix, sources, idx = inputs
-                mix = mix.to(device)
-                sources = sources.to(device)
+                    mix, clean, idx = inputs
+                    mix, clean = mix.to(device), clean.to(device)[None]
+
                 if config.norm:
                     mix_std = mix.std(-1, keepdim=True)
                     mix_mean = mix.mean(-1, keepdim=True)
@@ -40,26 +47,42 @@ def evaluate(config, model, dataset, savepath, epoch):
                     mix_std = mix_std.unsqueeze(1)
                     mix_mean = mix_mean.unsqueeze(1)
 
-                logits = model(mix.unsqueeze(0))
+                if config.model == '':
+                    logits = model(mix.unsqueeze(0))
+                else:
+                    logits = model(mix.unsqueeze(0), distance)
+
                 if config.norm:
                     logits = logits * mix_std + mix_mean
-                si_sdr, reordered_sources = criterion(logits, sources[None], return_est=True)
-                input_si_sdr = criterion(torch.stack([mix, mix], 0).unsqueeze(0), sources[None])
-                si_sdri = - (si_sdr - input_si_sdr).tolist() # loss is - si-sdr
+
+                mixcat = torch.stack([mix, mix], 0).unsqueeze(0)
+
+                si_snr = criterion(logits, clean, return_est=True if 'rir' not in dataset.task else False)
+                input_si_snr = criterion(mixcat, clean)
+                if 'rir' not in dataset.task:
+                    si_snr, reordered_sources = si_snr
+                si_snri = - (si_snr - input_si_snr).tolist()
+                if 'rir' in dataset.task:
+                    si_sdr, reordered_sources = criterion(logits, sources, return_est=True)
+                    input_si_sdr = criterion(mixcat, sources)
+                    si_sdri = - (si_sdr - input_si_sdr).tolist() # loss is - si-sdr
+                else:
+                    si_sdri = si_snri
                 
                 est_sources_np = reordered_sources.squeeze(0).cpu().data.numpy()
                 mix_np = mix.cpu().numpy()
                 
                 est_sources_np_normalized = normalize_estimates(est_sources_np, mix_np)
                 si_sdris.append(si_sdri)
-                pbar.set_postfix({'si_sdri':mean(si_sdris)})
+                si_snris.append(si_snri)
+                pbar.set_postfix({'si_sdri':mean(si_sdris), 'si_snri':mean(si_snris)})
                 
-                if idx in save_idx:
+                if savepath != '' and idx in save_idx:
                     local_save_dir = os.path.join(example_path, f"ex_{epoch}")
                     makedir(local_save_dir)
                     sf.write(os.path.join(local_save_dir, f"{idx}_mixture.wav"), mix_np, config.sr)
                     # Loop over the sources and estimates
-                    for src_idx, src in enumerate(sources):
+                    for src_idx, src in enumerate(sources[0]):
                         sf.write(os.path.join(local_save_dir, f"s{src_idx}.wav"), src.cpu().numpy(), config.sr)
                     for src_idx, est_src in enumerate(est_sources_np_normalized):
                         sf.write(
@@ -67,5 +90,5 @@ def evaluate(config, model, dataset, savepath, epoch):
                             est_src,
                             config.sr,
                         )
-    return mean(si_sdris)
+    return mean(si_sdris), mean(si_snris)
 
---
+++ evals_test.py
@@ -0,0 +1,59 @@
+import torch
+import torchaudio
+
+from model import ConvTasNet_v1
+from args import get_args
+from evals import evaluate
+from utils import get_device
+from data_utils import LibriMix
+
+import os
+from time import time
+
+
+class Test:
+    def __init__(self, config) -> None:
+        self.config = config
+        self.device = get_device()
+        if config.model == '':
+            self.model = torchaudio.models.ConvTasNet(msk_activate='relu')
+        elif config.model == 'v1':
+            self.model = ConvTasNet_v1()
+        if torch.cuda.device_count() > 1:
+            self.model = torch.nn.DataParallel(self.model)
+        self.model = self.model.to(self.device)
+        self.make_testdataset()
+    
+    def make_testdataset(self):
+        self.testset = LibriMix(
+            csv_dir=os.path.join(self.config.datapath, 'Libri2Mix/wav8k/min/test'),
+            config=self.config,
+            task='sep_clean',
+            sample_rate=self.config.sr,
+            n_src=self.config.speechnum,
+            segment=None,
+            return_id=True,
+        )
+        self.testset.df = self.testset.df[:1]
+
+    def test_evaluate(self):
+        testset = self.testset
+        si_sdri, si_snri = evaluate(self.config, self.model, testset, '', 1)
+
+    def run(self) -> None:
+        functions = [i for i in dir(self) if 'test_' in i]
+        test_st = time()
+        print('Test start')
+        for func in functions:
+            st = time()
+            print(func, 'testing...')
+            getattr(self, func)()
+            print(time() - st, 'seconds to test')
+        print(f'All test time is {time() - test_st}')
+
+if __name__ == '__main__':
+    config = get_args()
+    os.environ['CUDA_VISIBLE_DEVICES'] = config.gpus
+    test = Test(config)
+    test.run()
+
