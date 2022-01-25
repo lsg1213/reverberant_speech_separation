adb3df4c7b9e6d426893358de33bbaaf0e5b773d
Seungjin Lee <lsg0385@naver.com>
2022-01-25 06:19:16 +0000

    [add] distance limit between source receiver, clean label

--- make_dataset.py
+++ make_dataset.py
@@ -56,10 +56,16 @@ def get_RIR(config):
     att_diff = 15.0
     att_max = 60.0
     limit = 0.5 # 벽면에서부터 거리
+    distance_limit_between_src_rcv = 0.5 # 0.5 m
 
     room_sz = [random.random() * (w[1] - w[0]) + w[0], random.random() * (d[1] - d[0]) + d[0], random.random() * (h[1] - h[0]) + h[0]]
+
     pos_src = np.array([get_source_position(room_sz, limit=limit) for _ in range(config.nsrc)])
     pos_rcv = np.array([get_source_position(room_sz, limit=limit) for _ in range(config.mic)])
+    while (((pos_src - pos_rcv)**2).sum(-1) ** 0.5 < distance_limit_between_src_rcv).sum() > 0:
+        pos_src = np.array([get_source_position(room_sz, limit=limit) for _ in range(config.nsrc)])
+        pos_rcv = np.array([get_source_position(room_sz, limit=limit) for _ in range(config.mic)])
+
     T60 = random.random() * (T60[1] - T60[0]) + T60[0]
 
     beta = gpuRIR.beta_SabineEstimation(room_sz, T60) # Reflection coefficients
@@ -69,8 +75,9 @@ def get_RIR(config):
 
     RIRs = gpuRIR.simulateRIR(room_sz, beta, pos_src, pos_rcv, nb_img, Tmax, fs)
 
+    clean_RIRs = gpuRIR.simulateRIR(room_sz, np.zeros_like(beta), pos_src, pos_rcv, nb_img, Tmax, fs)
     distance = min(*pos_rcv[0], *(np.array(room_sz) - pos_rcv[0])) # the shortest distance between mic and wall
-    return RIRs, {'room': room_sz, 'pos_src': pos_src, 'pos_rcv': pos_rcv}, distance
+    return RIRs, {'room': room_sz, 'pos_src': pos_src, 'pos_rcv': pos_rcv, 'T60': T60}, distance, clean_RIRs
 
 
 def main(config):
@@ -92,7 +99,7 @@ def main(config):
         def generate(inputs):
             idx, (metric, mixture) = inputs
             sources = read_source(mixture) # [source1(time,), source2(time,)]
-            rir_function, rir_config, distance = get_RIR(config)
+            rir_function, rir_config, distance, clean_rir_function = get_RIR(config)
             rir_configs.append(rir_config)
             distances.append(distance)
             
@@ -100,24 +107,32 @@ def main(config):
             makedir('/'.join(mixture_save_path.split('/')[:-1]))
             rir_csv.at[idx, 'mixture_path'] = mixture_save_path
 
+            label_save_path = rir_csv.iloc[idx]['mixture_path'].replace(f'{prefix}mix_clean', f'{prefix}label_clean')
+            makedir('/'.join(label_save_path.split('/')[:-1]))
+
             # rir cross correlation operation
             sources = sources.cpu()
             rir_function = torch.from_numpy(rir_function.squeeze()).cpu()
+            clean_rir_function = torch.from_numpy(clean_rir_function.squeeze()).cpu()
             a_coefficient = F.pad(torch.ones((rir_function.shape[0], 1), device=rir_function.device, dtype=rir_function.dtype), (0, rir_function.shape[-1] - 1))
-            rir_sources = torchaudio.functional.lfilter(sources, a_coefficient, rir_function)
-
+            rir_sources = torchaudio.functional.lfilter(sources, torch.tensor(a_coefficient), rir_function)
+            rir_label = torchaudio.functional.lfilter(sources, torch.tensor(a_coefficient), clean_rir_function)
+            
             # rir normalization
             # rir_sources = rir_sources * sources.max(-1, keepdims=True)[0] / rir_sources.max(-1, keepdims=True)[0] / dis
 
             snr2 = 10 ** (metric['source_2_SNR'] / 20.)
             mixture_wave = torch.cat([rir_sources[:1], rir_sources[1:] * snr2])
+            label_wave = torch.cat([rir_label[:1], rir_label[1:] * snr2])
             
             # mixture save
             torchaudio.save(mixture_save_path, mixture_wave.cpu(), config.sr)
+            torchaudio.save(label_save_path, label_wave.cpu(), config.sr)
             print(mixture_save_path)
-        # list(map(generate, enumerate(zip(metric_csv.iloc, mixture_csv.iloc))))
+            
+        # list(map(generate, enumerate(zip(metric_csv.iloc, mixture_csv.iloc)))) # for debug
 
-        with ThreadPoolExecutor(cpu_count() // 2) as pool:
+        with ThreadPoolExecutor(cpu_count() // 4) as pool:
             list(pool.map(generate, enumerate(zip(metric_csv.iloc, mixture_csv.iloc))))
 
         rir_csv = pd.concat([rir_csv, pd.DataFrame(rir_configs)], 1)
