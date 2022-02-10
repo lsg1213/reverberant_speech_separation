e3090f716cb330b35827b58c44630a5b13d79a39
Seungjin Lee <lsg0385@naver.com>
2022-02-10 02:56:01 +0000

    [add] dereverb module test codes

--- args.py
+++ args.py
@@ -3,8 +3,9 @@ import argparse
 from torch.cuda import device_count
 
 
-def get_args():
-    args = argparse.ArgumentParser()
+def get_args(args: argparse.ArgumentParser = None):
+    if args is None:
+        args = argparse.ArgumentParser()
     args.add_argument('--name', type=str, default='')
     args.add_argument('--datapath', type=str, default='/root/bigdatasets/librimix')
     args.add_argument('--speechnum', type=int, default=2, choices=[2, 3])
@@ -12,7 +13,7 @@ def get_args():
     args.add_argument('--batch', type=int, default=16)
     args.add_argument('--task', type=str, default='')
     args.add_argument('--epoch', type=int, default=200)
-    args.add_argument('--max_patience', type=int, default=10)
+    args.add_argument('--max_patience', type=int, default=5)
     args.add_argument('--segment', type=int, default=3)
     args.add_argument('--lr', type=float, default=1e-3)
     args.add_argument('--clip_val', type=float, default=5.)
@@ -21,6 +22,7 @@ def get_args():
     args.add_argument('--gpus', type=str, default='-1')
     args.add_argument('--resume', action='store_true')
     args.add_argument('--norm', type=bool, default=True)
+    args.add_argument('--reverse', action='store_true')
     # args.add_argument('--norm', action='store_true')
     return args.parse_args()
     
--- dereverb_test.py
+++ dereverb_test.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentError
+import argparse
 from audioop import bias
 import os
 from turtle import forward
@@ -15,7 +16,7 @@ from torch.nn.modules.loss import MSELoss
 import torch.nn.functional as F
 import numpy as np
 from tensorboardX import SummaryWriter
-from asteroid.losses import pairwise_neg_sisdr
+from asteroid.losses import pairwise_neg_sisdr, PITLossWrapper
 from torch.utils.data import DataLoader
 from torch.optim import Adam
 from torch.optim.lr_scheduler import ReduceLROnPlateau
@@ -126,41 +127,50 @@ class MaskGenerator(torch.nn.Module):
         output = self.mask_activate(output)
         return output.view(batch_size, self.num_sources, self.output_dim, -1)
 
+
 class Lfilter(torch.nn.Module):
-    def __init__(self, input_channel, hidden_channel, kernel_size) -> None:
+    def __init__(self, config, input_channel, hidden_channel, kernel_size) -> None:
         super().__init__()
         self.input_channel = input_channel
         self.hidden_channel = hidden_channel
         self.kernel_size = kernel_size
-        
-        alpha = torch.rand((self.hidden_channel, 1), requires_grad=True)
-        beta = torch.rand((self.hidden_channel, 1), requires_grad=True)
-        self.kernel = []
-        for i in range(self.hidden_channel):
-            kernel = []
-            for j in range(1):
-                kernel.append(alpha[i, j] * torch.arange(0, beta[i, j].item(), beta[i, j].item() / self.kernel_size, dtype=torch.float32, requires_grad=True)[:self.kernel_size])
-            self.kernel.append(torch.stack(kernel, 0))
-        self.kernel = torch.nn.Parameter(torch.stack(self.kernel, 0))
-        self.kernel_weight = torch.nn.Parameter(torch.rand((self.hidden_channel, 1, self.kernel_size), requires_grad=True))
-        self.gru = torch.nn.GRU(self.input_channel, self.input_channel, batch_first=True, dropout=0.1, bidirectional=True)
-        self.prelu = torch.nn.PReLU()
+        self.config = config
 
+        if 'v2' not in self.config.model:
+            alpha = torch.rand((self.hidden_channel, 1), requires_grad=True)
+            beta = torch.rand((self.hidden_channel, 1), requires_grad=True)
+            self.kernel = []
+            for i in range(self.hidden_channel):
+                kernel = []
+                for j in range(1):
+                    kernel.append(alpha[i, j] * torch.arange(0, beta[i, j].item(), beta[i, j].item() / self.kernel_size, dtype=torch.float32, requires_grad=True)[:self.kernel_size])
+                self.kernel.append(torch.stack(kernel, 0))
+            self.kernel = torch.nn.Parameter(torch.stack(self.kernel, 0))
+            self.kernel_weight = torch.nn.Parameter(torch.rand((self.hidden_channel, 1, self.kernel_size), requires_grad=True))
+            self.prelu = torch.nn.PReLU()
+
+        if 'v1' not in self.config.model:
+            self.gru = torch.nn.GRU(self.input_channel, self.input_channel, batch_first=True, bidirectional=True)
+        
     def pad_for_lfilter(self, signal, filter_length):
         return F.pad(signal, (filter_length - 1, 0))
-
+            
     def forward(self, input: torch.Tensor):
-        batch, feat, time = input.shape
-        input = self.pad_for_lfilter(input, self.kernel_size)
-        pad_time = input.shape[-1]
-
         kernel = self.kernel * self.kernel_weight
-        input = input.reshape([-1, pad_time])
-        out = F.conv1d(input.unsqueeze(1).flip(-1), kernel).flip(-1)
-        out = out.sum(1).reshape((batch, feat, time))
-        out = self.prelu(out)
-        out = self.gru(out.transpose(-2, -1))[0].transpose(-2, -1)
-        out = out[:, :out.shape[1] // 2] + out[:, out.shape[1] // 2:]
+
+        if 'v2' in self.config.model:
+            out = input
+        else:
+            batch, feat, time = input.shape
+            input = self.pad_for_lfilter(input, self.kernel_size)
+            pad_time = input.shape[-1]
+            input = input.reshape([-1, pad_time])
+            out = F.conv1d(input.unsqueeze(1).flip(-1), kernel).flip(-1)
+            out = out.sum(1).reshape((batch, feat, time))
+            out = self.prelu(out)
+        if 'v1' not in self.config.model:
+            out = self.gru(out.transpose(-2, -1))[0].transpose(-2, -1)
+            out = out[:, :out.shape[1] // 2] + out[:, out.shape[1] // 2:]
         return out
 
 
@@ -220,6 +230,7 @@ class Lfilter(torch.nn.Module):
 class DereverbModule(ConvTasNet):
     def __init__(
         self,
+        config,
         num_sources: int = 2,
         # encoder/decoder parameters
         enc_kernel_size: int = 16,
@@ -233,8 +244,8 @@ class DereverbModule(ConvTasNet):
         msk_activate: str = "sigmoid",
     ):
         super(DereverbModule, self).__init__(num_sources, enc_kernel_size, enc_num_feats, msk_kernel_size, msk_num_feats, msk_num_hidden_feats, msk_num_layers, msk_num_stacks, msk_activate)
-
-        self.num_sources = num_sources
+        self.config = config
+        self.num_sources = 1 if self.config.test else num_sources
         self.enc_num_feats = enc_num_feats
         self.enc_kernel_size = enc_kernel_size
         self.enc_stride = enc_kernel_size // 2
@@ -247,15 +258,21 @@ class DereverbModule(ConvTasNet):
             padding=self.enc_stride,
             bias=False,
         )
+        self.mask_generator = MaskGenerator(
+            input_dim=enc_num_feats, # embedding
+            output_dim = enc_num_feats,
+            num_sources=num_sources,
+            kernel_size=msk_kernel_size,
+            num_feats=msk_num_feats,
+            num_hidden=msk_num_hidden_feats,
+            num_layers=msk_num_layers,
+            num_stacks=msk_num_stacks,
+            msk_activate=msk_activate,
+        )
 
         # 8000(sampling rate) * 0.5(T60 maximum value) / 8(encoded feature resolution)
         rir_func_length = np.ceil(8000 * 0.1 / 8).astype(np.int32)
-        # self.rir_function_generator = torch.nn.ModuleList([
-        #     torch.nn.Linear(3001, 1024),
-        #     torch.nn.Linear(1024, rir_func_length * 2),
-        #     torch.nn.Linear(rir_func_length * 2, rir_func_length),
-        # ])
-        self.lfilter = Lfilter(enc_num_feats, 64, rir_func_length)
+        self.lfilter = Lfilter(config, enc_num_feats, 64, rir_func_length)
 
         self.decoder = torch.nn.ConvTranspose1d(
             in_channels=enc_num_feats,
@@ -266,7 +283,7 @@ class DereverbModule(ConvTasNet):
             bias=False,
         )
 
-    def forward(self, input: torch.Tensor, test: bool = False) -> torch.Tensor:
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
         input = input.unsqueeze(1)
         """Perform source separation. Generate audio source waveforms.
 
@@ -292,20 +309,34 @@ class DereverbModule(ConvTasNet):
         batch_size, num_padded_frames = padded.shape[0], padded.shape[2]
         feats = self.encoder(padded)  # B, F, M
 
-        # rir predictor
-        rirfeats = feats
-        # for layer in self.rir_function_generator:
-        #     rirfeats = layer(rirfeats)
-        # dereverb_feats = lfilter(feats, rirfeats)
-        dereverb_feats = self.lfilter(rirfeats)
-        # dereverb_feats = F.sigmoid(dereverb_feats) * rirfeats
+        # separation module
+        if not self.config.test:
+            masked = self.mask_generator(feats) * feats.unsqueeze(1)  # B, S, F, M
+            separated_feat = masked.view(
+                batch_size * self.num_sources, self.enc_num_feats, -1 # + int(distance is not None)
+            )  # B*S, F, M
+            separated_feat = self.decoder(separated_feat)
+            separated_feat = separated_feat.view(
+                batch_size, self.num_sources, num_padded_frames
+            )
+            feats = separated_feat
+
+        # dereberberation module
+        dereverb_feats = self.lfilter(feats)
 
         masked = self.decoder(dereverb_feats)  # B*S, 1, L'
-        masked = masked.view(batch_size, num_padded_frames) # B, S, L'
+        output = masked.view(
+            batch_size, self.num_sources, num_padded_frames
+        )  # B, S, L'
 
         if num_pads > 0:
-            masked -= masked[..., :-num_pads]
-        return masked
+            output = output[..., :-num_pads]
+            if self.config.test:
+                separated_feat = separated_feat[..., :-num_pads]
+        if self.config.test:
+            return output
+        else:
+            return output, separated_feat
 
 
 def iterloop(config, writer, epoch, model: DereverbModule, criterion, dataloader, metric, optimizer=None, mode='train'):
@@ -315,6 +346,7 @@ def iterloop(config, writer, epoch, model: DereverbModule, criterion, dataloader
     output_scores = []
     input_scores = []
     rev_losses = []
+    sep_losses = []
     MSE = torch.nn.MSELoss()
     mses = []
 
@@ -328,9 +360,11 @@ def iterloop(config, writer, epoch, model: DereverbModule, criterion, dataloader
                 distance = distance.to(device)
             rev_sep = mix.to(device).transpose(1,2)
             clean_sep = clean.to(device).transpose(1,2)
-            # mix = rev_sep.sum(1)
-            mix = rev_sep[:,0]
-            clean_sep = clean_sep[:,:1]
+            if config.test:
+                mix = rev_sep[:,0]
+                clean_sep = clean_sep[:,:1]
+            else:
+                mix = rev_sep.sum(1)
 
             if config.norm:
                 mix_std = mix.std(-1, keepdim=True)
@@ -338,20 +372,29 @@ def iterloop(config, writer, epoch, model: DereverbModule, criterion, dataloader
                 mix = (mix - mix_mean) / mix_std
                 mix_std = mix_std
                 mix_mean = mix_mean
-            logits = model(mix)
+            outs = model(mix)
+            if config.test:
+                logits = outs
+            else:
+                logits, sep_logits = outs
             
 
             if config.norm:
                 mix = mix * mix_std + mix_mean
-                logits = logits * mix_std + mix_mean
-            rev_loss = criterion(logits.unsqueeze(1), clean_sep)
+                logits = logits * mix_std[:,None] + mix_mean[:,None]
+                if not config.test:
+                    sep_logits = sep_logits * mix_std[:,None] + mix_mean[:,None]
+            rev_loss = criterion(logits, clean_sep)
+            if not config.test:
+                sep_loss = criterion(sep_logits, rev_sep)
+
             if torch.isnan(rev_loss).sum() != 0:
                 print('nan is detected')
-                rev_loss = torch.where(torch.isnan(rev_loss) == False, rev_loss, torch.zeros_like(rev_loss))
+                exit()
 
-            loss = rev_loss.mean()
-            # loss = rev_loss + sep_loss
-
+            loss = rev_loss
+            if not config.test:
+                loss += sep_loss
 
             if mode == 'train':
                 optimizer.zero_grad()
@@ -361,17 +404,25 @@ def iterloop(config, writer, epoch, model: DereverbModule, criterion, dataloader
             losses.append(loss.item())
             if isinstance(rev_loss.tolist(), float):
                 rev_loss = rev_loss.unsqueeze(0)
+            if not config.test and isinstance(sep_loss.tolist(), float):
+                sep_loss = sep_loss.unsqueeze(0)
             rev_losses += rev_loss.tolist()
-            mse_score = MSE(logits, clean_sep.sum(1))
+            if not config.test:
+                sep_losses += sep_loss.tolist()
+            mse_score = MSE(logits, clean_sep)
             mses.append(mse_score.item())
+
             progress_bar_dict = {'mode': mode, 'loss': np.mean(losses), 'rev_loss': np.mean(rev_losses)}
+            if not config.test:
+                progress_bar_dict['sep_loss'] = np.mean(sep_losses)
             progress_bar_dict['mse_score'] = np.mean(mses)
 
-            writer.add_scalar(f'{mode}/loss', np.mean(losses), epoch)
-            writer.add_scalar(f'{mode}/mse_score', np.mean(mses), epoch)
-
-            output_score = - metric(logits.unsqueeze(1), clean_sep).squeeze()
-            input_score = - metric(mix.unsqueeze(1), clean_sep).squeeze()
+            output_score = - metric(logits, clean_sep).squeeze()
+            if config.test:
+                mixcat = mix.unsqueeze(1)
+            else:
+                mixcat = torch.stack([mix, mix], 1)
+            input_score = - metric(mixcat, clean_sep).squeeze()
             if isinstance(output_score.tolist(), float):
                 output_score = output_score.unsqueeze(0)
                 input_score = input_score.unsqueeze(0)
@@ -385,11 +436,19 @@ def iterloop(config, writer, epoch, model: DereverbModule, criterion, dataloader
 
             pbar.set_postfix(progress_bar_dict)
             if mode == 'val' and (num == 1 or num == 10):
-                torchaudio.save(f'sample/{config.name}_sample{num}_rev.wav', mix[:1].cpu(), 8000)
-                torchaudio.save(f'sample/{config.name}_sample{num}_clean.wav', clean_sep[0].cpu(), 8000)
-                torchaudio.save(f'sample/{config.name}_sample{num}_result.wav', logits[:1].cpu(), 8000)
+                torchaudio.save(f'sample/{config.name}_sample{num}_rev.wav', mix[0,None].cpu(), 8000)
+                torchaudio.save(f'sample/{config.name}_sample{num}_clean_1.wav', clean_sep[0,0,None].cpu(), 8000)
+                torchaudio.save(f'sample/{config.name}_sample{num}_clean_2.wav', clean_sep[0,1,None].cpu(), 8000)
+                torchaudio.save(f'sample/{config.name}_sample{num}_result_1.wav', logits[0,0,None].cpu(), 8000)
+                torchaudio.save(f'sample/{config.name}_sample{num}_result_2.wav', logits[0,1,None].cpu(), 8000)
             num += 1
 
+
+    writer.add_scalar(f'{mode}/loss', np.mean(losses), epoch)
+    writer.add_scalar(f'{mode}/rev_loss', np.mean(rev_losses), epoch)
+    if not config.test:
+        writer.add_scalar(f'{mode}/sep_loss', np.mean(sep_losses), epoch)
+    writer.add_scalar(f'{mode}/mse_score', np.mean(mses), epoch)
     if mode == 'train':
         return np.mean(losses)
     else:
@@ -402,7 +461,13 @@ def main(config):
     os.environ['CUDA_VISIBLE_DEVICES'] = config.gpus
     gpu_num = torch.cuda.device_count()
     config.batch *= max(gpu_num, 1)
-    config.model = 'derev'
+
+    # v1: lfilter
+    # v2: gru
+    # v3: lfilter+gru
+    if config.model not in ('v1','v2','v3'):
+        raise ValueError('model must be v1, v2, v3')
+    config.model = 'derev_' + config.model
     name = 'derev_' + (config.model if config.model is not '' else 'baseline')
     name += f'_{config.batch}'
     if config.model != '' and config.task == '':
@@ -415,6 +480,8 @@ def main(config):
         config.task += '_'
     if config.norm:
         name += '_norm'
+    if config.test:
+        name += '_test'
     config.name = name + '_' + config.name if config.name is not '' else ''
     config.tensorboard_path = os.path.join(config.tensorboard_path, config.name)
     writer = SummaryWriter(config.tensorboard_path)
@@ -434,7 +501,6 @@ def main(config):
         n_src=config.speechnum,
         segment=config.segment,
     )
-    import pdb; pdb.set_trace()
 
     val_set = LibriMix(
         csv_dir=os.path.join(config.datapath, 'Libri2Mix/wav8k/min/dev'),
@@ -469,7 +535,7 @@ def main(config):
         num_workers=gpu_num * (cpu_count() // 4),
     )
 
-    model = DereverbModule()
+    model = DereverbModule(config)
 
     optimizer = Adam(model.parameters(), lr=config.lr)
     scheduler = ReduceLROnPlateau(optimizer=optimizer, factor=0.5, patience=3, verbose=True)
@@ -480,12 +546,12 @@ def main(config):
     callbacks = []
     callbacks.append(EarlyStopping(monitor="val_score", mode="max", patience=config.max_patience, verbose=True))
     callbacks.append(Checkpoint(checkpoint_dir=os.path.join(savepath, 'checkpoint.pt'), monitor='val_score', mode='max', verbose=True))
-    metric = pairwise_neg_sisdr
+    metric = PITLossWrapper(pairwise_neg_sisdr)
     def mseloss():
         def _mseloss(logit, answer):
             return MSELoss(reduction='none')(logit, answer)
         return _mseloss
-    criterion = pairwise_neg_sisdr
+    criterion = PITLossWrapper(pairwise_neg_sisdr)
     if 'mse' in config.name:
         criterion = MSELoss()
     
@@ -557,4 +623,6 @@ def main(config):
     
 
 if __name__ == '__main__':
-    main(get_args())
+    args = argparse.ArgumentParser()
+    args.add_argument('--test', action='store_true')
+    main(get_args(args))
